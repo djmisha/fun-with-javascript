f2d7eaeaebc0282ea31ec1bd0e376f004991402e
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2019-12-25 14:33:44 -0800

    Update writeCode.js

--- writeCode.js
+++ writeCode.js
@@ -2,7 +2,7 @@ const writeCode = (hours, distractions) => {
   let codeHours = hours;
   codeHours = codeHours * 60; // convert hours to minutes
 
-let devDistractions = distractions;
+	let devDistractions = distractions;
   devDistractions = devDistractions * 15; // for every distraction means lost 15 min 
    
 	let productivity;
@@ -11,41 +11,16 @@ let devDistractions = distractions;
   let averageLinesofCode = 50;  // average lines of code written by a dev according to research
   let averageLinesofCodePerMinute = (averageLinesofCode / 6.5) / 60;
 
-  let writtenLinesOfCode;
-  writtenLinesOfCode = productivity * averageLinesofCodePerMinute;
+  let writtenLinesOfCode = productivity * averageLinesofCodePerMinute;
   writtenLinesOfCode = Math.round(writtenLinesOfCode);
   
   console.log(
-    `The developer coded for ${codeHours} minutes, and had ${devDistractions} minutes of disctrations. He wrote ${writtenLinesOfCode} lines of code,`
-   )
+    `The developer coded for ${codeHours} minutes, and had ${devDistractions} minutes of disctrations. He wrote about ${writtenLinesOfCode} lines of code,`
+   );
 
   return writtenLinesOfCode;  
-}
+};
 
 const misha = writeCode(6.5,3);
 
 misha;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
