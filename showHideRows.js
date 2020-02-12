f4ac55ae0cc12c395860f0ef676de30625db80b1
Misha Osinovskiy <misha.osinovskiy@gmail.com>
2020-02-11 16:26:44 -0800

    Update showHideRows.js

--- showHideRows.js
+++ showHideRows.js
@@ -1,32 +1,31 @@
  /* Loads more Media Center items on click of Load More button 6 times before it stops */
-    function showMoreMedia() {
-        let mediarows = $('.home__mediacentre .container .media__row');
-        let loadbutton = $('.load-more-mediacentre span');
-        let counter = 3;
+function showMoreMedia() {
+    let mediarows = $('.home__mediacentre .container .media__row');
+    let loadbutton = $('.load-more-mediacentre span');
+    let counter = 3;
 
-        function scanRowsandShowHide() {
-            for (let i = 0; i < mediarows.length; i++) {
-                if (i >= counter) {
-                    mediarows[i].classList.add('media__hidden');
-                }
-                else {
-                    mediarows[i].classList.remove('media__hidden');
-                    mediarows[i].classList.add('media__visible');
-                }
+    function scanRowsandShowHide() {
+        for (let i = 0; i < mediarows.length; i++) {
+            if (i >= counter) {
+                mediarows[i].classList.add('media__hidden');
             }
-            counter ++;
-            if (counter === 7 ) {
-                loadbutton.css('display', 'none')
+            else {
+                mediarows[i].classList.remove('media__hidden');
+                mediarows[i].classList.add('media__visible');
             }
-            return counter;
         }
+        counter ++;
+        if (counter === 7 ) {
+            loadbutton.css('display', 'none')
+        }
+        return counter;
+    }
 
+    scanRowsandShowHide();
+
+    $('.load-more-mediacentre span').on('click', function(event) {
+        event.preventDefault;
         scanRowsandShowHide();
-
-        $('.load-more-mediacentre span').on('click', function(event) {
-            event.preventDefault;
-            scanRowsandShowHide();
-        });
-    };
-
-    showMoreMedia();
+    });
+};
+showMoreMedia();
